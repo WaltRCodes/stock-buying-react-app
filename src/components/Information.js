23731e663fef2f522b4729451b45828de8f0cfe5
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-21 15:26:11 -0400

    made information.js

---
+++ src/components/Information.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+export default class Information extends Component {
+
+    componentDidMount() {
+        
+    }
+  
+  render() {
+    return (
+      <div>
+        <h1>Information</h1>
+      </div>
+      
+    )
+  }
+}
