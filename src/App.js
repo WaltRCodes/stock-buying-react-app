ecec7142fb333eb090918e4e4059c70d5450fdbd
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-21 15:30:22 -0400

    added the information component which draws the data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import Information from './components/Information';
 function App() {
   return (
     <div>
-      
+      <Information />
     </div>
   );
 }
