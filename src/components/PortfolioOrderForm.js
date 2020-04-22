79a5ec711278d4b13c8df1c5a9f37ee97aa06259
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-22 10:20:10 -0400

    updated the portfolio input form

--- src/components/PortfolioOrderForm.js
+++ src/components/PortfolioOrderForm.js
@@ -3,7 +3,10 @@ function PortfolioOrderForm(props) {
   return (
     <div>
         <form>
-        
+        <label>
+              Symbol
+              <input type="text" onChange={this.takeSymbol} placeholder="Symbol"/>
+          </label>
         </form>
     </div>
   );
