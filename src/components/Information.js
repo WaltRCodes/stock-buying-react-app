f398d1d0a62f8cc9f7458c3590027f444c7c08f8
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-22 11:49:12 -0400

    added balance arguement

--- src/components/Information.js
+++ src/components/Information.js
@@ -68,7 +68,7 @@ export default class Information extends Component {
     return (
       <div>
         <h1>Information</h1>
-        <PortfolioOrderForm takeSymbol={this.takeSymbol} takeQty={this.takeQty} buyStock={this.buyStock} stopOrderBlock={this.state.stopOrderBlock}/>
+        <PortfolioOrderForm balance={this.state.balance} takeSymbol={this.takeSymbol} takeQty={this.takeQty} buyStock={this.buyStock} stopOrderBlock={this.state.stopOrderBlock}/>
       </div>
       
     )
